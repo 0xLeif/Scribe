c7c56cb9d52240cba325e5adde5cc5a91aedd9be
Leif <zmeriksen@icloud.com>
2023-02-01 18:00:22 -0700

    Update Plugin to 1.0.0

--- Sources/Scribe/Plugins/ScribePlugin.swift
+++ Sources/Scribe/Plugins/ScribePlugin.swift
@@ -1,27 +1,4 @@
 import Plugin
 
 /// A protocol that defines a Scribe plugin.
-public protocol ScribePlugin: Plugin
-where Source == Scribe,
-      Output == Void,
-      Input == Scribe.PluginPayload {
-    /// Handles the input value.
-    ///
-    /// - Parameters:
-    ///   - value: The input value that the plugin will handle.
-    /// - Throws: Any errors that occur during handling of the input value.
-    func handle(value: Scribe.PluginPayload) async throws
-}
-
-/// Extension that provides a default implementation for the `keyPath` property of `ScribePlugin`.
-extension ScribePlugin {
-    /// A keypath that provides access to the `immutable` property of `Scribe`.
-    public var keyPath: WritableKeyPath<Scribe, ()> {
-        get { \.immutable }
-        set { }
-    }
-
-    public func handle(value: Scribe.PluginPayload, output: inout ()) async throws {
-        try await handle(value: value)
-    }
-}
+public protocol ScribePlugin: ImmutablePlugin where Source == Scribe, Input == Scribe.PluginPayload {}
